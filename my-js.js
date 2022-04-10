ea40e6bfe95c0aa48347ae9dac3eef15041a8fa8
Pav <pavan_1994@hotmail.co.uk>
2022-04-10 21:29:55 +0100

    v1.2

--- my-js.js
+++ my-js.js
@@ -1,1 +1,49 @@
-document.getElementById("floating").style.animation = "fading 2s infinite";
+const the_button = document.querySelector(".js-btn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close");
+
+document.addEventListener("DOMContentLoaded", () => {
+  the_button.addEventListener("click", handleClick);
+});
+
+function handleClick(event) {
+  modal.style.display = "block";
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+}
+
+const form = document.getElementById("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const enquiry = document.getElementById("enquiries");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const fullNameValue = fullName.value.trim();
+  const emailValue = email.value.trim();
+  const enquiryValue = enquiry.value.trim();
+
+  if (fullNameValue === "") {
+    // show error
+    //add class
+    setErrorFor(fullName, "Name cannot be blank");
+  } else {
+    // add success
+    setSuccessFor(fullName);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; //.form-control
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
