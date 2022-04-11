b2590dd30cdcca880c1ece9db0fcf5a343762bf1
Pav <pavan_1994@hotmail.co.uk>
2022-04-11 20:33:26 +0100

    v4

--- my-js.js
+++ my-js.js
@@ -74,18 +74,3 @@ function isEmail(email) {
     email
   );
 }
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  let myForm = document.getElementById("form");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => alert("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-document.querySelector("form").addEventListener("submit", handleSubmit);
